4487e792d1d96f1e741d930a64b84dd0c3a1c947
Diony Rosa <dhrosa@gmail.com>
2025-12-09 19:35:31 -0500

    Remove preload. Space and time requirement is too high (30s+).

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -7,36 +7,13 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import Navigation from "./Navigation";
 import NearestColorsPage from "./NearestColorsPage";
 import BlendPage from "./BlendPage";
-import { PreloadProgress } from "./PreloadProgress";
-import { preloadSubscribe, preloadSnapshot } from "./Preload";
-
-function LoadingScreen({ progress }: { progress: PreloadProgress }) {
-  return (
-    <>
-      <p>Loading...</p>
-      {progress.type == "starting" && <progress className="progress" />}
-      {progress.type == "preloading" && (
-        <progress
-          className="progress"
-          value={progress.numerator}
-          max={progress.denominator}
-        />
-      )}
-    </>
-  );
-}
 
 function Layout() {
-  const progress = useSyncExternalStore(preloadSubscribe, preloadSnapshot);
   return (
     <>
       <Navigation />
       <section className="section">
-        {progress.type == "complete" ? (
-          <Outlet />
-        ) : (
-          <LoadingScreen progress={progress} />
-        )}
+        <Outlet />
       </section>
     </>
   );
