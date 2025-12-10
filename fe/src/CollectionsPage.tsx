e51acf3ee35234d758420aed3ec383fc9904fa22
Diony Rosa <dhrosa@gmail.com>
2025-12-10 17:21:35 -0500

    Render collections as a table

--- fe/src/CollectionsPage.tsx
+++ fe/src/CollectionsPage.tsx
@@ -2,8 +2,9 @@ import { useRef, useState } from "react";
 import { Collection } from "./Collection";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Modal } from "./Modal";
+import { Symbol } from "./Symbol";
 
-function CollectionListItem({ collection }: { collection: Collection }) {
+function CollectionRow({ collection }: { collection: Collection }) {
   const [deleteModalActive, setDeleteModalActive] = useState(false);
   const queryClient = useQueryClient();
   const deleteMutation = useMutation({
@@ -15,40 +16,58 @@ function CollectionListItem({ collection }: { collection: Collection }) {
   });
 
   return (
-    <li>
-      {collection.name}
-      <button className="delete" onClick={() => setDeleteModalActive(true)} />
-      <Modal
-        active={deleteModalActive}
-        onClose={() => setDeleteModalActive(false)}
-      >
-        <div className="box">
-          <p className="block">
-            Are you sure you want to delete the <em>{collection.name}</em>{" "}
-            collection?
-          </p>
-          <p className="buttons block">
-            <button
-              className={`button is-danger ${deleteMutation.isPending && "is-loading"}`}
-              onClick={() => deleteMutation.mutate()}
-            >
-              Delete
-            </button>
-            <button
-              className="button"
-              onClick={() => setDeleteModalActive(false)}
-            >
-              Cancel
-            </button>
-            {deleteMutation.isError && (
-              <p className="help is-danger">
-                Error: {deleteMutation.error.message}
-              </p>
-            )}
-          </p>
-        </div>
-      </Modal>
-    </li>
+    <tr>
+      <td>{collection.name}</td>
+      <td>
+        <button className="button">
+          <span className="icon-text">
+            <span className="icon">
+              <Symbol name="edit" />
+            </span>
+            <span>Rename</span>
+          </span>
+        </button>
+        <button className="button" onClick={() => setDeleteModalActive(true)}>
+          <span className="icon-text">
+            <span className="icon">
+              <Symbol name="delete" />
+            </span>
+            <span>Delete</span>
+          </span>
+        </button>
+
+        <Modal
+          active={deleteModalActive}
+          onClose={() => setDeleteModalActive(false)}
+        >
+          <div className="box">
+            <p className="block">
+              Are you sure you want to delete the <em>{collection.name}</em>{" "}
+              collection?
+            </p>
+            <p className="buttons block">
+              <button
+                className={`button is-danger ${deleteMutation.isPending && "is-loading"}`}
+                onClick={() => deleteMutation.mutate()}
+              >
+                Delete
+              </button>
+              <button
+                className="button"
+                onClick={() => setDeleteModalActive(false)}
+              >
+                Cancel
+              </button>
+              {deleteMutation.isError && (
+                <p className="help is-danger">
+                  Error: {deleteMutation.error.message}
+                </p>
+              )}
+            </p>
+          </div>
+        </Modal>
+      </td>
+    </tr>
   );
 }
 
@@ -82,11 +101,19 @@ function CollectionList() {
 
   return (
     <>
-      <ul>
-        {collections.map((c) => (
-          <CollectionListItem key={c.name} collection={c} />
-        ))}
-      </ul>
+      <table className="collections table block">
+        <thead>
+          <tr>
+            <th>Collection Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collections.map((c) => (
+            <CollectionRow key={c.name} collection={c} />
+          ))}
+        </tbody>
+      </table>
 
       <input
         ref={inputRef}
