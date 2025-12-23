05ee431e568610db80cc31f73213ccb89a4beffd
Diony Rosa <dhrosa@gmail.com>
2025-12-23 11:16:47 -0500

    Card-based collections page

--- src/routes/collections.tsx
+++ src/routes/collections.tsx
@@ -7,6 +7,7 @@ import { Modal } from "../Modal";
 import { Symbol } from "../Symbol";
 import { Control, ErrorHelp, Field, Label } from "../Form";
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { SingleFloss } from "../Floss";
 
 export const Route = createFileRoute("/collections")({
   component: CollectionsPage,
@@ -15,10 +16,20 @@ export const Route = createFileRoute("/collections")({
 // Query key for list of all collections.
 const queryKey = ["collections"];
 
-// Render a Collection as a table row.
-function CollectionRow({ collection }: { collection: Collection }) {
-  const [deleteModalActive, setDeleteModalActive] = useState(false);
-  const [renameModalActive, setRenameModalActive] = useState(false);
+function FlossPreview({ flosses }: { flosses: SingleFloss[] }) {
+  const gradientArgs = flosses.map(({ color }) => color.display());
+  return (
+    <div
+      className="floss-preview"
+      style={{
+        background: `linear-gradient(to bottom right, ${gradientArgs.join(", ")})`,
+      }}
+    ></div>
+  );
+}
+
+function RenameButton({ collection }: { collection: Collection }) {
+  const [modalActive, setModalActive] = useState(false);
   const queryClient = useQueryClient();
 
   // Ref for the new name input element.
@@ -29,128 +40,133 @@ function CollectionRow({ collection }: { collection: Collection }) {
     mutationFn: async () => {
       await collection.rename(newNameRef.current.value);
       queryClient.invalidateQueries({ queryKey });
-      setRenameModalActive(false);
-    },
-  });
-
-  // Triggered by delete button.
-  const deleteMutation = useMutation({
-    mutationFn: async () => {
-      await collection.delete();
-      queryClient.invalidateQueries({ queryKey });
-      setDeleteModalActive(false);
+      setModalActive(false);
     },
   });
 
   return (
-    <tr>
-      <td>
-        <Link to="/collections/$name" params={{ name: collection.name }}>
-          {collection.name}
-        </Link>
-      </td>
-      <td>
-        {/* Actions */}
-        {/* Rename button */}
-        <button className="button" onClick={() => setRenameModalActive(true)}>
-          <span className="icon-text">
-            <span className="icon">
-              <Symbol name="edit" />
-            </span>
-            <span>Rename</span>
+    <>
+      <a className="card-footer-item" onClick={() => setModalActive(true)}>
+        <span className="icon-text">
+          <span className="icon">
+            <Symbol name="edit" />
           </span>
-        </button>
-        {/* Delete button */}
-        <button className="button" onClick={() => setDeleteModalActive(true)}>
-          <span className="icon-text">
-            <span className="icon">
-              <Symbol name="delete" />
-            </span>
-            <span>Delete</span>
-          </span>
-        </button>
-        {/* Rename button */}
-        <Modal
-          active={renameModalActive}
-          onClose={() => setRenameModalActive(false)}
-        >
-          <div className="box">
-            <Field>
-              <Label>New Name</Label>
-              <Control>
-                <input
-                  className="input"
-                  type="text"
-                  ref={newNameRef}
-                  defaultValue={collection.name}
-                />
-              </Control>
-            </Field>
-            <Field className="is-grouped">
-              <Control>
-                <button
-                  className={`button is-primary ${renameMutation.isPending && "is-loading"}`}
-                  onClick={() => renameMutation.mutate()}
-                >
-                  Submit
-                </button>
-              </Control>
-              <Control>
-                <button
-                  className="button"
-                  onClick={() => setRenameModalActive(false)}
-                >
-                  Cancel
-                </button>
-              </Control>
-              {renameMutation.isError && (
-                <ErrorHelp>
-                  {renameMutation.error.name == "ConstraintError"
-                    ? "Collection with that name already exists."
-                    : renameMutation.error.toString()}
-                </ErrorHelp>
-              )}
-            </Field>
-          </div>
-        </Modal>
-        {/* Deletion modal */}
-        <Modal
-          active={deleteModalActive}
-          onClose={() => setDeleteModalActive(false)}
-        >
-          <div className="box">
-            <p className="block">
-              Are you sure you want to delete the <em>{collection.name}</em>{" "}
-              collection?
-            </p>
-            <p className="buttons block">
+          <span>Rename</span>
+        </span>
+      </a>
+      <Modal active={modalActive} onClose={() => setModalActive(false)}>
+        <div className="box">
+          <Field>
+            <Label>New Name</Label>
+            <Control>
+              <input
+                className="input"
+                type="text"
+                ref={newNameRef}
+                defaultValue={collection.name}
+              />
+            </Control>
+          </Field>
+          <Field className="is-grouped">
+            <Control>
               <button
-                className={`button is-danger ${deleteMutation.isPending && "is-loading"}`}
-                onClick={() => deleteMutation.mutate()}
+                className={`button is-primary ${renameMutation.isPending && "is-loading"}`}
+                onClick={() => renameMutation.mutate()}
               >
-                Delete
+                Submit
               </button>
-              <button
-                className="button"
-                onClick={() => setDeleteModalActive(false)}
-              >
+            </Control>
+            <Control>
+              <button className="button" onClick={() => setModalActive(false)}>
                 Cancel
               </button>
-              {deleteMutation.isError && (
-                <ErrorHelp>{deleteMutation.error.message}</ErrorHelp>
-              )}
-            </p>
-          </div>
-        </Modal>
-      </td>
-    </tr>
+            </Control>
+          </Field>
+          {renameMutation.isError && (
+            <ErrorHelp>
+              {renameMutation.error.name == "ConstraintError"
+                ? "Collection with that name already exists."
+                : renameMutation.error.toString()}
+            </ErrorHelp>
+          )}
+        </div>
+      </Modal>
+    </>
   );
 }
 
-// Table row for creating a new collection.
-function NewCollectionRow() {
+function DeleteButton({ collection }: { collection: Collection }) {
+  const [modalActive, setModalActive] = useState(false);
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: async () => {
+      await collection.delete();
+      queryClient.invalidateQueries({ queryKey });
+      setModalActive(false);
+    },
+  });
+
+  return (
+    <>
+      <a className="card-footer-item" onClick={() => setModalActive(true)}>
+        <span className="icon-text">
+          <span className="icon">
+            <Symbol name="delete" />
+          </span>
+          <span>Delete</span>
+        </span>
+      </a>
+      <Modal active={modalActive} onClose={() => setModalActive(false)}>
+        <div className="box">
+          <p className="block">
+            Are you sure you want to delete the <em>{collection.name}</em>{" "}
+            collection?
+          </p>
+          <p className="buttons block">
+            <button
+              className={`button is-danger ${deleteMutation.isPending && "is-loading"}`}
+              onClick={() => deleteMutation.mutate()}
+            >
+              Delete
+            </button>
+            <button className="button" onClick={() => setModalActive(false)}>
+              Cancel
+            </button>
+            {deleteMutation.isError && (
+              <ErrorHelp>{deleteMutation.error.message}</ErrorHelp>
+            )}
+          </p>
+        </div>
+      </Modal>
+    </>
+  );
+}
+
+function CollectionCard({ collection }: { collection: Collection }) {
+  return (
+    <div className="card" style={{ width: "240px" }}>
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <FlossPreview flosses={collection.flosses} />
+        </figure>
+      </div>
+      <div className="card-header">
+        <div className="card-header-title">{collection.name}</div>
+      </div>
+
+      <div className="card-footer">
+        <RenameButton collection={collection} />
+        <DeleteButton collection={collection} />
+      </div>
+    </div>
+  );
+}
+
+function NewCollectionButton() {
   const queryClient = useQueryClient();
   const [newName, setNewName] = useState("");
+  const [modalActive, setModalActive] = useState(false);
 
   // Triggered by add button.
   const addMutation = useMutation({
@@ -158,41 +174,54 @@ function NewCollectionRow() {
       await Collection.create(newName);
       setNewName("");
       queryClient.invalidateQueries({ queryKey });
+      setModalActive(false);
     },
   });
 
   return (
-    <tr>
-      <td>
-        <input
-          className="input"
-          type="text"
-          placeholder="New collection name..."
-          value={newName}
-          onChange={(e) => setNewName(e.target.value)}
-        />
-        {addMutation.error && (
-          <ErrorHelp>
-            {addMutation.error.name == "ConstraintError"
-              ? "Collection with that name already exists."
-              : addMutation.error.toString()}
-          </ErrorHelp>
-        )}
-      </td>
-      <td>
-        <button
-          className={`button ${addMutation.isPending && "is-loading"}`}
-          onClick={() => addMutation.mutate()}
-        >
-          <span className="icon-text">
-            <span className="icon">
-              <Symbol name="add" />
-            </span>
-            <span>Create</span>
-          </span>
-        </button>
-      </td>
-    </tr>
+    <>
+      <button
+        className="block button is-success"
+        onClick={() => setModalActive(true)}
+      >
+        <span className="icon is-small">
+          <Symbol name="add" />
+        </span>
+        <span>Create New Collection</span>
+      </button>
+      <Modal active={modalActive} onClose={() => setModalActive(false)}>
+        <div className="box">
+          <Field>
+            <Label>New Collection Name</Label>
+            <Control>
+              <input
+                className="input"
+                type="text"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Control>
+              <button
+                className={`button is-success ${addMutation.isPending && "is-loading"}`}
+                onClick={() => addMutation.mutate()}
+              >
+                Submit
+              </button>
+            </Control>
+          </Field>
+          {addMutation.error && (
+            <ErrorHelp>
+              {addMutation.error.name == "ConstraintError"
+                ? "Collection with that name already exists."
+                : addMutation.error.toString()}
+            </ErrorHelp>
+          )}
+        </div>
+      </Modal>
+    </>
   );
 }
 
@@ -213,24 +242,17 @@ export function CollectionsPage() {
   }
 
   return (
-    <>
-      <div className="collections-page box">
-        <p className="title is-4">My Collections</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Collection Name</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <NewCollectionRow />
-            {collections.map((c) => (
-              <CollectionRow key={c.name} collection={c} />
-            ))}
-          </tbody>
-        </table>
+    <div className="collections-page">
+      <p className="title is-4">My Collections</p>
+
+      <NewCollectionButton />
+
+      <div className="block collections ">
+        {collections &&
+          collections.map((c) => (
+            <CollectionCard key={c.name} collection={c} />
+          ))}
       </div>
-    </>
+    </div>
   );
 }
